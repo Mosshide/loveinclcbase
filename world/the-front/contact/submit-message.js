c981aed44eddb3f343228a37512b95b58979b866
Wyatt Lee Sushinsky <wyattsushi@gmail.com>
2024-02-23 22:44:16 -0800

    self hosted img for emails

--- world/the-front/contact/submit-message.js
+++ world/the-front/contact/submit-message.js
@@ -8,12 +8,11 @@ export default async function submitMessage(req, user) {
         ...req.body.ticket
     }
 
-    // THE FOLLOWING IMAGE SOURCES ARE TEMPORARY AND WILL NEED TO BE REPLACED WITH THE ACTUAL IMAGE SOURCES (https://www.loveincoflewiscounty.org/img/banner.jpg)
     // render the data so it is readable in the email
     // the message will need to be rendered as HTML, with line breaks converted to <br> tags
     await req.db.SendMail.send("exec.director@loveincoflewiscounty.org", `New Message for Love INC of Lewis County`, `
         <div style="border-radius:5px; width: 90%; padding: 20px; background: rgb(242,242,245);" class="custom-email-body">
-            <img src="https://www.loveincoflewiscounty.org/build/image/2648.png?w=1500&h=425&fit=crop-48-54&s=b68fb57b38a818af110c9a98f5069ada" alt="Love INC of Lewis County Logo" style="width: 100%; height: auto;">
+            <img src="https://loveinclcbase-99e96083d1e2.herokuapp.com/img/banner.jpg" alt="Love INC of Lewis County Logo" style="width: 100%; height: auto;">
             <h1>New Message From: ${newMessage.name}</h1> 
             User's E-mail: ${newMessage.email}
             <br>
@@ -42,7 +41,7 @@ export default async function submitMessage(req, user) {
     // send a confirmation email to the user
     await req.db.SendMail.send(newMessage.email, `Message Sent to Love INC of Lewis County`, `
         <div style="border-radius:5px; width: 90%; padding: 20px; background: rgb(242,242,245);" class="custom-email-body">
-            <img src="https://www.loveincoflewiscounty.org/build/image/2648.png?w=1500&h=425&fit=crop-48-54&s=b68fb57b38a818af110c9a98f5069ada" alt="Love INC of Lewis County Logo" style="width: 100%; height: auto;">
+            <img src="https://loveinclcbase-99e96083d1e2.herokuapp.com/img/banner.jpg" alt="Love INC of Lewis County Logo" style="width: 100%; height: auto;">
             <h1>Thank you for reaching out!</h1> 
             Your message was sent to Love INC of Lewis County on ${new Date(newMessage.dateCreated).toLocaleDateString()}.
             <br>
