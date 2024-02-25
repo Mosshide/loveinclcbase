752cbe19028869a42d30716fe9ce6fed22f4a131
Wyatt Lee Sushinsky <wyattsushi@gmail.com>
2024-02-25 14:48:03 -0800

    favicon

--- loveinclcbase.js
+++ loveinclcbase.js
@@ -4,4 +4,7 @@ const __dirname = fileURLToPath(new URL('./world', import.meta.url));
 
 import nkjvBible from "./globals/NKJV.js";
 
-const notherBaseFS = new NotherBaseFS(__dirname, { nkjvBible }, { siteTitle: "Love INC of Lewis County" });
+const notherBaseFS = new NotherBaseFS(__dirname, { nkjvBible }, { 
+    siteTitle: "Love INC of Lewis County",
+    favicon: fileURLToPath(new URL('./public/img/logo.ico', import.meta.url)),
+ });
