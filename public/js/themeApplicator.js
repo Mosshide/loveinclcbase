83a3f34e7586203ad25c38f4f81d8d2f8d6d9b1b
Wyatt Lee Sushinsky <wyattsushi@gmail.com>
2024-04-27 15:10:46 -0700

    mobile nav and header font

--- public/js/themeApplicator.js
+++ public/js/themeApplicator.js
@@ -17,6 +17,9 @@ class ThemeApplicator {
         // add the font awesome css to the head
         this.$head.append($(`<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">`));
 
+        //ADD FONTS
+        this.$head.append($(`<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">`));
+
         // add the nav button for mobile
         this.$navButton = $("<button id='nav-button'>Navigation<i class='fa-solid fa-bars'></i></button>").prependTo(this.$main);
         this.$navButton.click(() => this.enableNavMobile());
